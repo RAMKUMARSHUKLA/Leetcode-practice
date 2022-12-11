2451285fb86ce96373509d785d0af18ffd09967a
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2022-12-11 11:39:37 +0530

    Time: 12 ms (24.45%), Space: 49.7 MB (57.32%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,42 +2,56 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int s1=nums1.length;
         int s2=nums2.length;
-        double[] temp=new double[s1+s2];
-        int i=0,j=0,k=0;
+//         double[] temp=new double[s1+s2];
+//         int i=0,j=0,k=0;
         
-        //sorting
-        while(i<s1&&j<s2)
+//         //sorting
+//         while(i<s1&&j<s2)
+//         {
+//             if(nums1[i]<=nums2[j])
+//             {
+//                 temp[k]=nums1[i];
+//                 i++;
+//                 k++;
+//             }
+//             else
+//             {
+//                 temp[k]=nums2[j];
+//                 j++;
+//                 k++;
+//             }
+//         }
+        
+//         if(i>=s1)
+//         {
+//             for(int t=j;t<s2;t++)
+//             {
+//                 temp[k]=nums2[t];
+//                 k++;
+//             }
+//         }
+//         if(j>=s2)
+//         {
+//             for(int t=i;t<s1;t++)
+//             {
+//                 temp[k]=nums1[t];
+//                 k++;
+//             }
+//         }
+        double temp[]=new double[s1+s2];
+        int k=0;
+        for(int i=0;i<s1;i++)
         {
-            if(nums1[i]<=nums2[j])
-            {
-                temp[k]=nums1[i];
-                i++;
-                k++;
-            }
-            else
-            {
-                temp[k]=nums2[j];
-                j++;
-                k++;
-            }
+         temp[k]=nums1[i];
+         k++;
         }
         
-        if(i>=s1)
+         for(int i=0;i<s2;i++)
         {
-            for(int t=j;t<s2;t++)
-            {
-                temp[k]=nums2[t];
-                k++;
-            }
+         temp[k]=nums2[i];
+         k++;
         }
-        if(j>=s2)
-        {
-            for(int t=i;t<s1;t++)
-            {
-                temp[k]=nums1[t];
-                k++;
-            }
-        }
+        Arrays.sort(temp);
         //just for checking printing temp array
         /*for(int t=0;t<k;t++)
             System.out.print(temp[t]+" ");*/
