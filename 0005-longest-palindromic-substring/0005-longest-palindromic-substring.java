bed13db8c34c075c4ee750f0058d3522849e790c
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2022-12-11 22:15:22 +0530

    Time: 230 ms (30.67%), Space: 47.5 MB (39.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,42 @@
 class Solution {
     public String longestPalindrome(String s) {
-            int[] maxStart = new int[1], maxEnd = new int[1]; // use array in order to pass by reference instead of pass by value
+//             int[] maxStart = new int[1], maxEnd = new int[1]; // use array in order to pass by reference instead of pass by value
         
-        for (int i = 0; i < s.length()-1; i++) {
-            extend(s, i, i, maxStart, maxEnd);    
-            extend(s, i, i+1, maxStart, maxEnd);
-        }
+//         for (int i = 0; i < s.length()-1; i++) {
+//             extend(s, i, i, maxStart, maxEnd);    
+//             extend(s, i, i+1, maxStart, maxEnd);
+//         }
         
-        return s.substring(maxStart[0], maxEnd[0]+1);
-    }
+//         return s.substring(maxStart[0], maxEnd[0]+1);
+//     }
     
-    private void extend(String s, int i, int j, int[] maxStart, int[] maxEnd) {
-        // loop until meet invalid match
-        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
-            i--; j++; 
-        }
+//     private void extend(String s, int i, int j, int[] maxStart, int[] maxEnd) {
+//         // loop until meet invalid match
+//         while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+//             i--; j++; 
+//         }
         
-        i++; j--; // back to the last valid match
+//         i++; j--; // back to the last valid match
         
-        if (j - i + 1 > maxEnd[0] - maxStart[0] + 1) {
-            maxStart[0] = i;
-            maxEnd[0] = j;
+//         if (j - i + 1 > maxEnd[0] - maxStart[0] + 1) {
+//             maxStart[0] = i;
+//             maxEnd[0] = j;
+//         }
+        
+         String result = "";
+        int len = 0;
+        boolean[][] isPali = new boolean[s.length()][s.length()];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || isPali[i + 1][j - 1])) {
+                    isPali[i][j] = true;
+                    if (j - i + 1 > len) {
+                        result = s.substring(i, j + 1);
+                        len = j - i + 1;
+                    }
+                }
+            }
         }
+        return result;
     }
 }
