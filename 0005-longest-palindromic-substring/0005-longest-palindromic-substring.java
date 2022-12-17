99fdc9b416b84c1bcaaf902c68e7365e5ace8dd0
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2022-12-17 14:44:44 +0530

    Time: 183 ms (34.50%), Space: 45.3 MB (44.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,42 +1,56 @@
 class Solution {
-    public String longestPalindrome(String s) {
-//             int[] maxStart = new int[1], maxEnd = new int[1]; // use array in order to pass by reference instead of pass by value
-        
-//         for (int i = 0; i < s.length()-1; i++) {
-//             extend(s, i, i, maxStart, maxEnd);    
-//             extend(s, i, i+1, maxStart, maxEnd);
-//         }
-        
-//         return s.substring(maxStart[0], maxEnd[0]+1);
-//     }
-    
-//     private void extend(String s, int i, int j, int[] maxStart, int[] maxEnd) {
-//         // loop until meet invalid match
-//         while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
-//             i--; j++; 
-//         }
-        
-//         i++; j--; // back to the last valid match
-        
-//         if (j - i + 1 > maxEnd[0] - maxStart[0] + 1) {
-//             maxStart[0] = i;
-//             maxEnd[0] = j;
-//         }
-        
-         String result = "";
-        int len = 0;
-        boolean[][] isPali = new boolean[s.length()][s.length()];
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = i; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || isPali[i + 1][j - 1])) {
-                    isPali[i][j] = true;
-                    if (j - i + 1 > len) {
-                        result = s.substring(i, j + 1);
-                        len = j - i + 1;
-                    }
+    public String longestPalindrome(String str) {
+         boolean[][] dp = new boolean[str.length()][str.length()];
+        int start = 0, end = 0;
+
+        /**
+         * The loop should be Diagonal wise Starting with gap = 0 till string length
+         * First loop it should cover [(0,0), (1,1), (2,2), (3,3), (4,4)]
+         * Second Loop - (0,1), (1,2), (2,3) & (3,4)
+         *
+         * Like this diagonally it should travel and solve the problem.
+         */
+        for (int gap = 0; gap < str.length(); gap++) {
+            for (int i = 0, j = gap; j < dp[0].length; i++, j++) {
+
+                /**
+                 *    Case-1 : For Gap-0, i.e. position [(0,0), (1,1), (2,2)] values are always TRUE.
+                 *             Because start and end position is same. So, it consists of one character only.
+                 */
+                if (gap == 0) {
+                    dp[i][j] = true;
+                    continue;
                 }
+                /**
+                 *    Case-2 : For Gap-1, i.e. position (0,1), (1,2), (2,3) & (3,4) Compare the Start position and End position.
+                 *             Because there are only two characters are there.
+                 */
+                if (gap == 1 && str.charAt(i) == str.charAt(j)) {
+                    dp[i][j] = true;
+                }
+                /**
+                 *  Case-3 : For all rest of the position check the start and end position
+                 *           If true check for middle part of the string i.e. check for (start+1) till (End -1) position.
+                 *           Which can be obtained from left diagonal value i.e. for (i,j) check for (i+1 , j -1)
+                 */
+                else if (str.charAt(i) == str.charAt(j) && dp[i + 1][j - 1] == true) {
+                    dp[i][j] = true;
+                }
+
+                /**
+                 * Keeping track of the Start end End position of the Palindromic String.
+                 * As Gap is increasing we are not doing any comparison with the previous range.
+                 * With increasing of the Gap value if the substring is palindrome then it will be
+                 * the bigger substring.
+                 */
+                if (dp[i][j]) {
+                    start = i;
+                    end = j;
+                }
+
             }
         }
-        return result;
+
+        return str.substring(start, end + 1);
     }
 }
