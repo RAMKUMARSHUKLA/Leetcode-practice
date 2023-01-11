58e494de14bdf1e19dea59bb6891a84cdb308b65
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-01-11 11:30:13 +0530

    Time: 1 ms (100.00%), Space: 43.4 MB (58.48%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,20 +1,20 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+    int leftSum=0;;
     int totalSum=0;
-    int leftSum=0;
-    for(int ele: nums)
-    {
-        totalSum+=ele;
-    }
-    for(int i=0;i<nums.length; leftSum+=nums[i++])
-    {
-      if(leftSum*2==totalSum-nums[i])
-      {
-          return i;
-      }
         
+    for(int sum: nums)
+    {
+        totalSum+=sum;
     }
-    return -1;
+    for(int i=0;i<nums.length;leftSum+=nums[i++])
+    {
+       if(leftSum*2==totalSum-nums[i])
+       {
+           return i;
+       }
+    }
+        return -1;
         
   }
 }
