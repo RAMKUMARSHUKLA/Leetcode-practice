3f56313572e278405417e8f87d228d739d67e60b
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-01-12 11:36:02 +0530

    Time: 6 ms (88.88%), Space: 41.7 MB (99.20%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,10 +1,6 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length())
-        {
-            return false;
-        }
-        
+       if(s.length()==0) return false;
         HashMap<Character,Character> map1=new HashMap<>();
         HashMap<Character,Boolean> map2=new HashMap<>();
         
@@ -12,8 +8,6 @@ class Solution {
         {
             char ch1=s.charAt(i);
             char ch2=t.charAt(i);
-            
-            //checking for key at ch1
             if(map1.containsKey(ch1))
             {
                 if(map1.get(ch1)!=ch2)
@@ -21,21 +15,16 @@ class Solution {
                     return false;
                 }
             }
-            //no key for map1 then come here
             else
             {
                 if(map2.containsKey(ch2))
                 {
                     return false;
                 }
-                else
-                {
-                    map1.put(ch1,ch2);
-                    map2.put(ch2,true);
-                }
+                map1.put(ch1,ch2);
+                map2.put(ch2,true);
             }
         }
         return true;
-        
     }
 }
