fadb11ab2cdec8d392c90824b9f30965ad451fb9
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-01-12 11:54:28 +0530

    Time: 16 ms (96.70%), Space: 39.3 MB (65.28%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,19 +3,22 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-         int l=1;
-        int h=n;
-
-        while(l<=h){
-            int mid=(l+(h-l)/2);
+        int low=1;
+        int high=n;
         
-            if(isBadVersion(mid)==false){
-                l=mid+1;
+        while(low<=high)
+        {
+            int mid=(low+(high-low)/2);
+            
+            if(isBadVersion(mid)==false)
+            {
+                low=mid+1;
             }
-            else{
-                h=mid-1;
+            else
+            {
+                high=mid-1;
             }
         }
-        return l;
+        return  low;
     }
 }
