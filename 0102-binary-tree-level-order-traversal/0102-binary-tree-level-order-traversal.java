75e82da1546cb31c57b71124508392ecea551dce
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-01-14 15:36:12 +0530

    Time: 1 ms (83.97%), Space: 42.4 MB (81.97%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -16,31 +16,24 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         
-      //Using DFS to traverse all the node
-        List<List<Integer>> res = new ArrayList<>();
-        //base case
+         List<List<Integer>> res = new ArrayList<>();
         if (root == null)
             return res;
-        visit(res, root, 0);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+            }
+            res.add(level);
+        }
         return res;
     }
-	
-	public void visit(List<List<Integer>> res, TreeNode root, int level) {
-		if (root == null)
-			return;
-		List<Integer> curr;
-		if (level >= res.size()) {
-            //reach the new level, create a new list to add in res
-			curr = new ArrayList<>();
-			curr.add(root.val);
-			res.add(curr);
-		} else {
-            //level already visited
-			curr = res.get(level); 
-			curr.add(root.val); 
-		}
-        //visit children
-		visit(res, root.left, level + 1);
-		visit(res, root.right, level + 1);
-	}
 }
