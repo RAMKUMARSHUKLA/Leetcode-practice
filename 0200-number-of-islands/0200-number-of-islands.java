c93ebf479ef714a6a02b2e108aac2bc3b0817efc
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-01-16 11:00:00 +0530

    Time: 3 ms (92.26%), Space: 50.9 MB (74.42%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,27 @@
 class Solution {
     public int numIslands(char[][] grid) {
          int count = 0;
+        boolean visited[][]=new boolean[grid.length][grid[0].length];
         
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
+                if (grid[i][j] == '1' && visited[i][j]==false) {
                     count++;
-                    clearRestOfLand(grid, i, j);
+                    clearRestOfLand(grid, i, j,visited);
                 }
             }
         }
         return count;
     }
     
-    private void clearRestOfLand(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') return;
+    private void clearRestOfLand(char[][] grid, int i, int j,boolean visited[][]) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0' || visited[i][j]==true) return;
         
         grid[i][j] = '0';
-        clearRestOfLand(grid, i+1, j);
-        clearRestOfLand(grid, i-1, j);
-        clearRestOfLand(grid, i, j+1);
-        clearRestOfLand(grid, i, j-1);
+        clearRestOfLand(grid, i+1, j,visited);
+        clearRestOfLand(grid, i-1, j,visited);
+        clearRestOfLand(grid, i, j+1,visited);
+        clearRestOfLand(grid, i, j-1,visited);
         return;
     }
 }
