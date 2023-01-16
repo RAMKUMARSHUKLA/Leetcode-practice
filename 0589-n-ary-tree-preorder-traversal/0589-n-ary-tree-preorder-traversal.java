cebde9a4499fea83112b463bb6807342a2f886fb
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-01-16 22:13:53 +0530

    Time: 2 ms (54.12%), Space: 43.2 MB (58.48%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -19,40 +19,17 @@ class Node {
 
 class Solution {
     public List<Integer> preorder(Node root) {
-     // // To store the output array...
-     //    List<Integer> output = new ArrayList<Integer>();
-     //    // Base case: if the tree is empty...
-     //    if (root == null) return output;
-     //    // Create a stack of Node and push root to it...
-     //    Stack<Node> st = new Stack<>();
-     //    st.push(root);
-     //    // Traverse till stack is not empty...
-     //    while (!st.isEmpty()) {
-     //        // Pop a Node from the stack and add it to the output list...
-     //        Node node = st.pop();
-     //        output.add(node.val);
-     //        // Push all of the child nodes of the node into the stack from right to left...
-     //        // Push from right to left to get the right preorder traversal...
-     //        for (int idx = node.children.size() - 1; idx >= 0; idx--) {
-     //            Node child = node.children.get(idx);
-     //            st.push(child);
-     //        }
-     //    }
-     //    return output;
-        
-// ----------------->>>>>>>>>       Recursive Solution   <<<<<<<<----------------------- 
-        
-       if(root==null)
+    if(root==null)
     {
         return new ArrayList<>();
     }
-     List<Integer> res=new ArrayList<>();
-     res.add(root.val);
-     
-     for(Node child: root.children)     
-     {
-         res.addAll(preorder(child));
-     }
-    return res;
+    
+    List<Integer> arr=new ArrayList<>();
+        arr.add(root.val);
+    for(Node child:root.children)
+    {
+        arr.addAll(preorder(child));
+    }
+    return arr;
 }
 }
