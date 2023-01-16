09e95d01b36f39740d552355a43cd65b625147e0
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-01-16 22:05:42 +0530

    Time: 1 ms (100.00%), Space: 59.3 MB (60.20%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int smallIndexValue=Integer.MAX_VALUE;
-        int totalProfit=0;
+       int smallIndex=Integer.MAX_VALUE;
+       int totalProfit=0;
         
-        for(int i=0;i<prices.length;i++)
-        {
-            if(prices[i]<smallIndexValue)
-            {
-                smallIndexValue=prices[i];
-            }
-            
-            if(totalProfit<prices[i]-smallIndexValue)
-            {
-                totalProfit=prices[i]-smallIndexValue;
-            }
-        }
+       for(int i=0;i<prices.length;i++)
+       {
+           if(prices[i]<smallIndex)
+           {
+               smallIndex=prices[i];
+           }
+           
+           if(totalProfit<prices[i]-smallIndex)
+           {
+               totalProfit=prices[i]-smallIndex;
+           }
+       }
         return totalProfit;
         
         
