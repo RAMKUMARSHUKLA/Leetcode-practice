19e2cc648e74c2306a696d50ca14b1ee171cf9b2
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-01-16 22:10:21 +0530

    Time: 1 ms (100.00%), Space: 40.7 MB (70.46%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,16 +1,17 @@
 class Solution {
     public int longestPalindrome(String s) {
-       int charCount[]=new int[128];
-        for(char c:s.toCharArray())
-        {
-            charCount[c]++;
-        }
-        
+      int charCount[]=new int[128];
         int len=0;
+        
+      for(char c:s.toCharArray())
+      {
+          charCount[c]++;
+      }
+      
         for(int i:charCount)
         {
             len+=2*(i/2);
         }
-        return len<s.length()?len+1:len;
+        return len<s.length()? len+1:len;
     }
 }
