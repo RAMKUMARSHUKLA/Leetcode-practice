9d3f821b9e0eb5314f1592636ece55d2542f1408
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-03-04 20:03:33 +0530

    Time: 4 ms (97.65%), Space: 43.5 MB (66.19%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,32 +1,45 @@
 class Solution {
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
-         int rows = heights.length, cols = heights[0].length;
-        boolean[][] pac = new boolean[rows][cols];
-        boolean[][] atl = new boolean[rows][cols];
-        
-        for (int col = 0; col< cols; col++){
-            dfs(0, col, rows, cols, pac, heights[0][col], heights);
-            dfs(rows-1, col,rows, cols, atl, heights[rows-1][col], heights);
-        }
-        for (int row = 0; row<rows; row++){
-            dfs(row, 0,rows, cols, pac, heights[row][0], heights);
-            dfs(row, cols-1,rows, cols, atl, heights[row][cols-1], heights);
-        }
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++){
-                if (pac[i][j] && atl[i][j])
-                    result.add(Arrays.asList(i,j));
-            }
-        return result;
+    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+    List<List<Integer>> ans = new ArrayList<>();
+    if(matrix.length < 1) return ans;
+    int m = matrix.length, n = matrix[0].length;
+    int[][] pacific = new int[m][n];
+    int[][] atlantic = new int[m][n];
+
+    for(int col = 0; col < n; col++) {
+        fnc(matrix, 0, col, Integer.MIN_VALUE, pacific);
+        fnc(matrix, m - 1, col, Integer.MIN_VALUE, atlantic);
     }
-    private void dfs(int row, int col, int rows, int cols, boolean[][] visited, int prevHeight, int[][] heights){
-        if (row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col] || prevHeight > heights[row][col])
-            return;
-        visited[row][col]= true;
-        dfs(row+1, col, rows, cols, visited, heights[row][col], heights);
-        dfs(row-1, col, rows, cols, visited, heights[row][col], heights);
-        dfs(row, col+1, rows, cols, visited, heights[row][col], heights);
-        dfs(row, col-1, rows, cols, visited, heights[row][col], heights);
+
+    for(int row = 0; row < m; row++) {
+        fnc(matrix, row, 0, Integer.MIN_VALUE, pacific);
+        fnc(matrix, row, n - 1, Integer.MIN_VALUE, atlantic);
+    }
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(pacific[i][j] == 1 && atlantic[i][j] == 1) {
+                List<Integer> v = new ArrayList<>();
+                v.add(i);
+                v.add(j);
+                ans.add(v);
+            }
+        }
+    }
+
+    return ans;
+}
+
+private void fnc(int[][] matrix, int i, int j, int prev, int[][] ocean) {
+    if(i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length) return;
+    if(ocean[i][j] == 1) return;
+    if(matrix[i][j] < prev) return;
+
+    ocean[i][j] = 1;
+
+    fnc(matrix, i + 1, j, matrix[i][j], ocean); 
+    fnc(matrix, i - 1, j, matrix[i][j], ocean);
+    fnc(matrix, i, j + 1, matrix[i][j], ocean);
+    fnc(matrix, i, j - 1, matrix[i][j], ocean);
     }
 }
