9c4455fb93a16e28b6527be1a81e4ae920a46906
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-03-07 09:04:03 +0530

    Time: 4 ms (97.61%), Space: 43.4 MB (80.60%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,45 +1,56 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] matrix) {
-    List<List<Integer>> ans = new ArrayList<>();
-    if(matrix.length < 1) return ans;
-    int m = matrix.length, n = matrix[0].length;
-    int[][] pacific = new int[m][n];
-    int[][] atlantic = new int[m][n];
-
-    for(int col = 0; col < n; col++) {
-        fnc(matrix, 0, col, Integer.MIN_VALUE, pacific);
-        fnc(matrix, m - 1, col, Integer.MIN_VALUE, atlantic);
+        List<List<Integer>> res=new ArrayList<>();
+    if(matrix.length<1)
+        return res;
+    int m=matrix.length; int n=matrix[0].length;
+    int pacific[][]=new int[m][n];
+    int atlantic[][]=new int[m][n];
+    
+    for(int col=0;col<n;col++)
+    {
+        fnc(matrix,0,col,Integer.MIN_VALUE,pacific);
+        fnc(matrix,m-1,col,Integer.MIN_VALUE,atlantic);
     }
-
-    for(int row = 0; row < m; row++) {
-        fnc(matrix, row, 0, Integer.MIN_VALUE, pacific);
-        fnc(matrix, row, n - 1, Integer.MIN_VALUE, atlantic);
+        
+      for(int row=0;row<m;row++)
+    {
+        fnc(matrix,row,0,Integer.MIN_VALUE,pacific);
+        fnc(matrix,row,n-1,Integer.MIN_VALUE,atlantic);
     }
-
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            if(pacific[i][j] == 1 && atlantic[i][j] == 1) {
-                List<Integer> v = new ArrayList<>();
+    
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(pacific[i][j]==1 && atlantic[i][j]==1)
+            {
+                List<Integer> v=new ArrayList<>();
                 v.add(i);
                 v.add(j);
-                ans.add(v);
+                res.add(v);
             }
         }
     }
-
-    return ans;
+        return res;
+        
 }
-
-private void fnc(int[][] matrix, int i, int j, int prev, int[][] ocean) {
-    if(i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length) return;
-    if(ocean[i][j] == 1) return;
-    if(matrix[i][j] < prev) return;
-
-    ocean[i][j] = 1;
-
-    fnc(matrix, i + 1, j, matrix[i][j], ocean); 
-    fnc(matrix, i - 1, j, matrix[i][j], ocean);
-    fnc(matrix, i, j + 1, matrix[i][j], ocean);
-    fnc(matrix, i, j - 1, matrix[i][j], ocean);
+    
+    public void fnc(int matrix[][],int i,int j,int prev,int ocean[][])
+    {
+        if(i<0 || j<0 || i>=matrix.length || j>=matrix[0].length)
+        {
+            return;
+        }
+        if(ocean[i][j]==1) return ;
+        if(matrix[i][j]<prev) return;
+        ocean[i][j]=1;
+        
+        fnc(matrix,i+1,j,matrix[i][j],ocean);
+        fnc(matrix,i-1,j,matrix[i][j],ocean);
+        fnc( matrix,i,j+1,matrix[i][j],ocean);
+        fnc(matrix,i,j-1,matrix[i][j],ocean);
+        
+        
     }
 }
