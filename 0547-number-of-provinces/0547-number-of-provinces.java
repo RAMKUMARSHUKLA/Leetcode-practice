79e93405847a8473858896cde09db98eb244fe73
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-06-04 22:10:19 +0530

    Time: 1 ms (98.97%), Space: 44.3 MB (14.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,52 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-            int n = isConnected.length;
-        int provinces = 0;
-        boolean[] visited = new boolean[n];
+//             int n = isConnected.length;
+//         int provinces = 0;
+//         boolean[] visited = new boolean[n];
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                provinces++;
-                dfs(isConnected, visited, i);
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 provinces++;
+//                 dfs(isConnected, visited, i);
+//             }
+//         }
+
+//         return provinces;
+//     }
+
+//     private void dfs(int[][] isConnected, boolean[] visited, int node) {
+//         visited[node] = true;
+//         for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+//             if (isConnected[node][neighbor] == 1 && !visited[neighbor]) {
+//                 dfs(isConnected, visited, neighbor);
+//             }
+//         }
+        
+        int n=isConnected.length;
+        int province=0;
+        boolean isVisited[]=new boolean[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            if(!isVisited[i])
+            {
+                province++;
+                dfs(isConnected,isVisited,i);
             }
         }
-
-        return provinces;
+        return province;
     }
-
-    private void dfs(int[][] isConnected, boolean[] visited, int node) {
-        visited[node] = true;
-        for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
-            if (isConnected[node][neighbor] == 1 && !visited[neighbor]) {
-                dfs(isConnected, visited, neighbor);
-            }
+    public static void dfs(int[][] isConnected,boolean isVisited[], int node)
+    {
+        isVisited[node]=true;
+        
+        for(int neighbour=0; neighbour<isConnected.length; neighbour++)
+        {
+          if(isConnected[node][neighbour]==1 && !isVisited[neighbour])
+          {
+              dfs(isConnected,isVisited,neighbour);
+          }
         }
+       
     }
 }
