6af5a1dd5b870c8742ca1055412757e1a0107425
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-07-01 09:23:06 +0530

    Time: 2 ms (61.43%), Space: 40.4 MB (91.57%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-      
+      int k=0;
       if(s.length()==0)
       {
           return true;
       }
-    int j=0;
-    for(int i=0;i<t.length();i++)
-    {
-        if(t.charAt(i)==s.charAt(j))
-        {
-            j++;
-        }
-        if(j==s.length())
-        {
-            return true;
-        }
-    }
-    return false;
+        
+      for(int i=0;i<t.length();i++)
+      {
+          if(s.charAt(k)==t.charAt(i))
+          {
+              k++;
+          }
+          if(k==s.length())
+          {
+              return true;
+          }
+      }
+        return false;
     }
 }
