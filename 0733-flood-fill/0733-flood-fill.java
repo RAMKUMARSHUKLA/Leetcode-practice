6c164e01b92dc6b83ee83f7b420b9a0b29bc0d39
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-07-15 09:37:03 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (56.09%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
-        if(image[sr][sc]==color) return image;
-        fill(image,sr,sc,color,image[sr][sc]);
+      if(color==image[sr][sc]) return image;
+      fill(image,sr,sc,color,image[sr][sc]);
         return image;
     }
+    
     public static void fill(int[][] image,int sr,int sc,int color,int initColor)
     {
-        //checking if outside Array index
         if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length) return;
         
-        
-        //Already updated
         if(initColor!=image[sr][sc]) return;
-        
-        //Above condition fails then update with new color
         image[sr][sc]=color;
         
         fill(image,sr-1,sc,color,initColor);
