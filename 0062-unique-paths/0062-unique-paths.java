fcdb4ae5c0e6f1a285b3d9062f50d8363264f464
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-07-19 08:57:33 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (27.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
      
-      return helper(m,n,new int[m+1][n+1]);
-        
+     return uniquePathsGrid(m,n,new int[m+1][n+1]);
     }
     
-    private int helper(int m,int n,int arr[][])
+    public int uniquePathsGrid(int m,int n,int[][] arr)
     {
         if(m==0 || n==0) return 0;
-        if (m==1 || n==1) return 1;
+        if(m==1 || n==1) return 1;
         
-        //Already filled or already visited
         if(arr[m][n]!=0) return arr[m][n];
-        arr[m][n]=helper(m-1,n,arr)+helper(m,n-1,arr);
-        return arr[m][n];
+        
+         arr[m][n]=uniquePathsGrid(m-1,n,arr)+uniquePathsGrid(m,n-1,arr);
+         
+         return arr[m][n];
     }
         
     
