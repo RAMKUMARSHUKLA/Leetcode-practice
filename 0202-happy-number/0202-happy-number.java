38c4b0c3944acc40b6741402de17a6033cb39f0b
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-07-24 10:34:49 +0530

    Time: 1 ms (91.23%), Space: 40.1 MB (13.86%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean isHappy(int n) {
-   HashSet<Integer> set=new HashSet<>();
-    while(n!=1)
-    {
-        if(!set.contains(n))
-        {
-            set.add(n);
-        }
-        else
-        {
-            return false;
-        }
+ HashSet<Integer> set=new HashSet<>();
         
-        int sum=0;
-        
-        while(n!=0)
+        while(n!=1)
         {
-            sum+=(n%10)*(n%10);
-            n=n/10;
+            if(!set.contains(n))
+            {
+                set.add(n);
+            }
+            else
+            {
+                return false;
+            }
+            
+            int sum=0;
+            while(n!=0)
+            {
+                sum+=(n%10)*(n%10);
+                n=n/10;
+            }
+            n=sum;
         }
-        n=sum;
-    }
         return true;
-
     }
 }
