2a12c44307d314136e6a447ad6ec0fb11c140f90
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-07-25 09:07:03 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (94.55%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,27 +1,28 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-         List<Integer> res = new LinkedList<>(); 
-        if (matrix == null || matrix.length == 0) return res;
-        int n = matrix.length, m = matrix[0].length;
-        int up = 0,  down = n - 1;
-        int left = 0, right = m - 1;
-        while (res.size() < n * m) {
-            for (int j = left; j <= right && res.size() < n * m; j++)
-                res.add(matrix[up][j]);
-            
-            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++)
-                res.add(matrix[i][right]);
-                     
-            for (int j = right; j >= left && res.size() < n * m; j--)
-                res.add(matrix[down][j]);
-                        
-            for (int i = down - 1; i >= up + 1 && res.size() < n * m; i--) 
-                res.add(matrix[i][left]);
-                
-            left++; right--; up++; down--; 
+          List<Integer> res = new ArrayList<>();
+    if (matrix == null || matrix.length == 0) return res;
+
+    int rows = matrix.length, cols = matrix[0].length;
+    int left = 0, right = cols - 1, up = 0, down = rows - 1;
+
+    while (res.size() < rows * cols) {
+        for (int j = left; j <= right; j++) res.add(matrix[up][j]);
+        up++;
+        
+        for (int i = up; i <= down; i++) res.add(matrix[i][right]);
+        right--;
+        
+        if (res.size() < rows * cols) {
+            for (int j = right; j >= left; j--) res.add(matrix[down][j]);
+            down--;
         }
+        
+        if (res.size() < rows * cols) {
+            for (int i = down; i >= up; i--) res.add(matrix[i][left]);
+            left++;
+        }
+    }
         return res;
-
-
     }
 }
