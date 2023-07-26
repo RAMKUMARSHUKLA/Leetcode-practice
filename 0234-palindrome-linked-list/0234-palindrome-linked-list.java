8493e53dfe19df54df6b5b0ed4390dd95ae0d110
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-07-26 09:43:52 +0530

    Time: 17 ms (18.86%), Space: 63.7 MB (5.59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,29 +9,33 @@
  * }
  */
 class Solution {
+    ListNode pleft;
     public boolean isPalindrome(ListNode head) {
-       ListNode slow = head, fast = head, prev, temp;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+      pleft=head;
+      return isPalindromeHelper(head);
+        
+    }
+    
+    public boolean isPalindromeHelper(ListNode right)
+    {
+        if(right==null)
+        {
+            return true;
         }
-        prev = slow;
-        slow = slow.next;
-        prev.next = null;
-        while (slow != null) {
-            temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
+        boolean rresult=isPalindromeHelper(right.next);
+        if(rresult==false)
+        {
+            return false;
         }
-        fast = head;
-        slow = prev;
-        while (slow != null) {
-            if (fast.val != slow.val) return false;
-            fast = fast.next;
-            slow = slow.next;
+        else if(pleft.val!=right.val)
+        {
+            return false;
         }
-        return true;
+        else
+        {
+            pleft=pleft.next;
+            return true;
+        }
         
     }
 }
