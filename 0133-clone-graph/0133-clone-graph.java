fb06e602a8ad086182a096b63774b0ec9f577018
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-07-31 10:44:01 +0530

    Time: 27 ms (33.13%), Space: 41.7 MB (91.97%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,27 +20,30 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-     if (node == null) {
-            return null;
-        }
-
-        Map<Node, Node> visited = new HashMap<>();
-        return dfsClone(node, visited);
+     if(node==null)
+     {
+         return null;
+     }
+        Map<Node,Node> visited=new HashMap<>();
+        return dfsClone(node,visited);
     }
-
-    private Node dfsClone(Node node, Map<Node, Node> visited) {
-        if (visited.containsKey(node)) {
+    public Node dfsClone(Node node,Map<Node,Node> visited)
+    {
+        if(visited.containsKey(node))
+        {
             return visited.get(node);
         }
-
-        Node newNode = new Node(node.val);
-        visited.put(node, newNode);
-
-        for (Node neighbor : node.neighbors) {
-            Node newNeighbor = dfsClone(neighbor, visited);
-            newNode.neighbors.add(newNeighbor);
+        
+        Node newNode=new Node(node.val);
+        visited.put(node,newNode);
+        
+        for(Node neighbor:node.neighbors)
+        {
+            Node node1=dfsClone(neighbor,visited);
+            newNode.neighbors.add(node1);
         }
-
+        
         return newNode;
     }
+    
 }
