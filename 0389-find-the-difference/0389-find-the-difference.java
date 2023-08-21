9f582e9184fb318f811bd9e8ca404816d4965462
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-08-21 11:51:17 +0530

    Time: 1 ms (100.00%), Space: 40.7 MB (81.64%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,28 +1,41 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         
-        HashMap<Character,Integer> map=new HashMap<>();
+//         HashMap<Character,Integer> map=new HashMap<>();
         
+//         for(char ch:t.toCharArray())
+//         {
+//             map.put(ch,map.getOrDefault(ch,0)+1);
+//         }
+        
+//         for(char ch: s.toCharArray())
+//         {
+//             if(map.containsKey(ch))
+//             {
+//                 map.put(ch,map.get(ch)-1);
+//             }
+//         }
+        
+//         for(Map.Entry<Character,Integer> entry:map.entrySet())
+//         {
+//             if(entry.getValue()>0)
+//             {
+//                 return entry.getKey();
+//             }
+//         }
+//     return '\0';
+        
+        char num=0;
+        
+        for(char ch:s.toCharArray())
+        {
+            //Converts ch to ASCII value it. for a->97 and then does XOR operation
+            num^=ch;
+        }
         for(char ch:t.toCharArray())
         {
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            num^=ch;
         }
-        
-        for(char ch: s.toCharArray())
-        {
-            if(map.containsKey(ch))
-            {
-                map.put(ch,map.get(ch)-1);
-            }
-        }
-        
-        for(Map.Entry<Character,Integer> entry:map.entrySet())
-        {
-            if(entry.getValue()>0)
-            {
-                return entry.getKey();
-            }
-        }
-    return '\0';
+        return num;
     }
 }
