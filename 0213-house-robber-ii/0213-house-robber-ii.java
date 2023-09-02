e7e8c075af233b614f80e59c9c8ef5e7f4d9e151
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-09-02 12:27:42 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (83.83%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
-            return nums[0];
-        }
-        // Calculate the maximum amount if you rob the first house and exclude the last house
-        int max1 = robLinear(nums, 0, n - 2);
-        // Calculate the maximum amount if you exclude the first house and consider the last house
-        int max2 = robLinear(nums, 1, n - 1);
-        // Return the maximum of the two scenarios
-        return Math.max(max1, max2);
+      int n=nums.length;
+      if(n==1)
+      {
+          return nums[0];
+      }
+        
+    int max1=maxRob(nums,0,n-2);
+    int max2=maxRob(nums,1,n-1);
+    return Math.max(max1,max2);
     }
     
-    // Helper function to calculate maximum amount for a linear sequence of houses
-    private int robLinear(int[] nums, int start, int end) {
-        int prevMax = 0;
-        int currMax = 0;
-        for (int i = start; i <= end; i++) {
-            int temp = currMax;
-            currMax = Math.max(prevMax + nums[i], currMax);
-            prevMax = temp;
-        }
+    public int maxRob(int nums[],int start,int end)
+    {
+        int currMax=0;
+       int prevMax=0;
+       for(int i=start;i<=end;i++)
+       {
+           int temp=currMax;
+           currMax=Math.max(prevMax+nums[i],currMax);
+           prevMax=temp;
+       }
         return currMax;
     }
 }
