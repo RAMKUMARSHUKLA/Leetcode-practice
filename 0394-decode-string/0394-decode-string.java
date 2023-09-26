3a5b2282e8ed80ece2e33fe55b3eec3ad8fb3214
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-09-26 10:02:59 +0530

    Time: 6 ms (9.91%), Space: 40.8 MB (34.47%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,47 +1,51 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> countStack=new Stack<>();
-        Stack<String> resStack=new Stack<>();
-        String res="";
-        int i=0;
-        while(i<s.length())
-        {
-            if(Character.isDigit(s.charAt(i)))
-            {
-                int count=0;
-                while(Character.isDigit(s.charAt(i)))
-                {
-                  count=count*10+(s.charAt(i)-'0');
-                i++;  
-                }
-                countStack.push(count);
-                
-            }
-            else if(s.charAt(i)=='[')
-            {
-                resStack.push(res);
-                res="";
-                i++;
-            }
-            else if(s.charAt(i)==']')
-            {
-                StringBuilder temp=new StringBuilder(resStack.pop());
-                int repeatTimes=countStack.pop();
-                
-                for(int j=0;j<repeatTimes;j++)
-                {
-                    temp.append(res);
-                }
-                
-                res=temp.toString();
-                i++;
-            }
-            else
-            {
-                res+=s.charAt(i++);
-                
-            }
-        }
+       Stack<Integer> countStack=new Stack<>();
+       Stack<String> resStack=new Stack<>();
+        
+       String res="";
+       int i=0;
+       while(i<s.length())
+       {
+           
+           if(Character.isDigit(s.charAt(i)))
+           {
+               int count=0;
+               while(Character.isDigit(s.charAt(i)))
+               {
+                   count=count*10+(s.charAt(i)-'0');
+                   i++; 
+               }
+               
+               countStack.push(count);
+           }
+           
+          
+           else if(s.charAt(i)=='[')
+           {
+              resStack.push(res);
+              res="";
+              i++;
+           }
+           
+           else if(s.charAt(i)==']')
+           {
+               StringBuilder stringBuilder=new StringBuilder(resStack.pop());
+               int len=countStack.pop();
+               for(int j=0;j<len;j++)
+               {
+                   stringBuilder.append(res);
+               }
+               res=stringBuilder.toString();
+               i++;
+           }
+           
+           else
+           {
+               res+=s.charAt(i++);
+               
+           }
+       }
         return res;
     }
 }
