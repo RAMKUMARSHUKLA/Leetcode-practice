e9bfe35cc92b44ca66bee13ddc0c26a9034d8673
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-09-30 09:30:50 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (31.05%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,46 +1,46 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-    List<Integer> res=new ArrayList<>();
         
-    int minRow=0,minCol=0,maxRow=matrix.length-1,maxCol=matrix[0].length-1;
-    int tle=(maxRow+1)*(maxCol+1);
-    int count=1;
-while(count<=tle)
-    {
-        for(int i=minCol; i<=maxCol&& count<=tle; i++)
-        {
-            res.add(matrix[minRow][i]);
-            count++;
-        }
-        minRow++;
+        List<Integer> res=new ArrayList<>();
+        int minRow=0,minCol=0;
+        int maxRow=matrix.length-1,maxCol=matrix[0].length-1;
         
-        for(int i=minRow; i<=maxRow&& count<=tle; i++)
-        {
-            res.add(matrix[i][maxCol]);
-                        count++;
-
-        }
-        maxCol--;
+        int tle=(maxRow+1)*(maxCol+1);
+        int count=1;
         
-        for(int i=maxCol; i>=minCol&& count<=tle; i--)
+        while(count<=tle)
         {
-            res.add(matrix[maxRow][i]);
-                        count++;
-
+            for(int i=minCol;i<=maxCol && count<=tle;i++)
+            {
+                res.add(matrix[minRow][i]);
+                count++;
+            }
+            minRow++;
+            
+             for(int i=minRow; i<=maxRow && count<=tle;i++)
+            {
+                res.add(matrix[i][maxCol]);
+                count++;
+            }
+            maxCol--;
+            
+             for(int i=maxCol;i>=minCol && count<=tle;i--)
+            {
+                res.add(matrix[maxRow][i]);
+                count++;
+            }
+            maxRow--;
+            
+             for(int i=maxRow;i>=minRow && count<=tle;i--)
+            {
+                res.add(matrix[i][minCol]);
+                count++;
+            }
+            minCol++;
         }
-        maxRow--;
+        return res;
         
-        for(int i=maxRow; i>=minRow&& count<=tle; i--)
-        {
-            res.add(matrix[i][minCol]);
-                        count++;
-
-        }
-        minCol++;
-    }
-
-return res;
-
+        
 
     }
 }
