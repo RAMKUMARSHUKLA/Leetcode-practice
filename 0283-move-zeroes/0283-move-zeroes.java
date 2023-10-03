5b44abb5ecd1e98b29f954c29bca9cd51083289f
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-10-03 09:05:59 +0530

    Time: 1 ms (100.00%), Space: 45.3 MB (23.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        for(int num:nums)
-        {
-            if(num!=0)
-            {
-                nums[i]=num;
-                 i++;
-            }
-           
-        }
+       int i=0;
+       for(int num:nums)
+       {
+           if(num!=0)
+           {
+               nums[i]=num;
+               i++;
+           }
+       }
         while(i<nums.length)
         {
             nums[i]=0;
             i++;
         }
+       
        
     }
 }
