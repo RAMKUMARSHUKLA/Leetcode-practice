6aba6679bff28968ee7fddac2744080245f7440b
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-10-10 09:02:38 +0530

    Time: 5 ms (44.84%), Space: 41.9 MB (7.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,31 +1,31 @@
 class Solution {
        
-    String[] dial={"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    List<String> ans=new ArrayList<>();
+    String dial[]={"1","2","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    ArrayList<String> list=new ArrayList<>();
+    
     public List<String> letterCombinations(String digits) {
-     if(digits.isEmpty())
+     if(digits.isEmpty()) 
      {
          return new ArrayList<>();
-     }
-        
-    helper("",0,digits);
-        return ans;
+     }  
+     helper("",0,digits);
+        return list;
     
     
     }
 public void helper(String comb,int index,String digits)
 {
-    if(index==digits.length())
-    {
-        ans.add(comb);
+   if(index==digits.length())
+   {
+       list.add(comb);
         return;
     }
-    String letters=dial[digits.charAt(index)-'0'];
-   for(char ch:letters.toCharArray())
-   {
-       helper(comb+ch,index+1,digits);
-   }
     
+    String ans=dial[digits.charAt(index)-'0'];
+    for(char ch:ans.toCharArray())
+    {
+        helper(comb+ch,index+1,digits);
+    }
 }
     
 }
