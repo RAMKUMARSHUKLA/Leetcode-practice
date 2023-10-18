89854d0cc8b956673c4e9eb501ffa26d6ca3b3c7
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-10-18 08:55:39 +0530

    Time: 9 ms (87.26%), Space: 44.2 MB (54.10%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,14 +1,18 @@
 class Solution {
     public int partitionString(String s) {
-       int hash[] = new int[26];
-        int count = 0;
-        for(int i = 0 ; i < s.length(); i ++){
-            if(hash[s.charAt(i) - 'a'] == 1){
+       int hash[]=new int[26];
+        int count=0;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(hash[c-'a']==1)
+            {
                 count++;
-                hash = new int[26];
+                hash= new int[26];
             }
-            hash[s.charAt(i)-'a']++;
+            hash[c-'a']++;
         }
-        return count+1;  
+        return count+1;
     }
 }
