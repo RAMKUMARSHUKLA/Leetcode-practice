a4a2ccfbe69e491b6c68fa4b70df79664fea03ad
RAM KUMAR SHUKLA <34932402+MAYANKSHUKLA123@users.noreply.github.com>
2023-10-20 09:15:15 +0530

    Time: 6 ms (50.25%), Space: 44.5 MB (27.69%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,34 +1,45 @@
 class Solution {
     public String convert(String s, int numRows) {
-if (numRows == 1 || numRows >= s.length()) {
-            return s;
+// if (numRows == 1 || numRows >= s.length()) {
+//             return s;
+//         }
+
+//         StringBuilder[] rows = new StringBuilder[numRows];
+//         for (int i = 0; i < numRows; i++) {
+//             rows[i] = new StringBuilder();
+//         }
+
+//         int direction = 1; // 1 for down, -1 for up
+//         int currentRow = 0;
+
+//         for (char c : s.toCharArray()) {
+//             rows[currentRow].append(c);
+
+//             if (currentRow == 0) {
+//                 direction = 1;
+//             } else if (currentRow == numRows - 1) {
+//                 direction = -1;
+//             }
+
+//             currentRow += direction;
+//         }
+
+//         StringBuilder result = new StringBuilder();
+//         for (StringBuilder row : rows) {
+//             result.append(row);
+//         }
+
+//         return result.toString();
+        
+        int index = 0;
+        StringBuilder[] st = new StringBuilder[numRows];
+        for (int i = 0; i < st.length; i++) st[i] = new StringBuilder();
+        while (index < s.length()) {
+            for (int i = 0; i < numRows && index < s.length(); i++) st[i].append(s.charAt(index++));
+            for (int i = numRows - 2; i > 0 && index < s.length(); i--) st[i].append(s.charAt(index++));
         }
-
-        StringBuilder[] rows = new StringBuilder[numRows];
-        for (int i = 0; i < numRows; i++) {
-            rows[i] = new StringBuilder();
-        }
-
-        int direction = 1; // 1 for down, -1 for up
-        int currentRow = 0;
-
-        for (char c : s.toCharArray()) {
-            rows[currentRow].append(c);
-
-            if (currentRow == 0) {
-                direction = 1;
-            } else if (currentRow == numRows - 1) {
-                direction = -1;
-            }
-
-            currentRow += direction;
-        }
-
         StringBuilder result = new StringBuilder();
-        for (StringBuilder row : rows) {
-            result.append(row);
-        }
-
+        for (int i = 0; i < st.length; i++) result.append(st[i]);
         return result.toString();
         
     }
